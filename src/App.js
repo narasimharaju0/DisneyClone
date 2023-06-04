0d0c00e7d1ee8183fa69d2fc846c4edde0624abb
narasimharaju0 <narasimharajupaidi@gmail.com>
2023-06-04 19:27:36 +0530

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.js 	new file:   src/components/Detail.js 	modified:   src/components/Home.js 	new file:   src/components/Login.js 	new file:   src/components/Movies.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,33 @@ import { Counter } from './features/counter/Counter';
 import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
+import { BrowserRouter as Router,
+   Switch,
+   Route, 
+   Link 
+  } from 'react-router-dom';
+  import Detail from './components/Detail';
+  import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
-       <Header/>
-       <Home/>
+    <Router>
+         <Header/>
+         <Switch>
+          <Route path='/detail'>
+               <Detail/>
+          </Route>
+
+          <Route path='/login'>
+              <Login/>
+          </Route>
+
+          <Route path='/'>
+              <Home/>
+           </Route>
+         </Switch>
+    </Router>
     </div>
   );
 }
